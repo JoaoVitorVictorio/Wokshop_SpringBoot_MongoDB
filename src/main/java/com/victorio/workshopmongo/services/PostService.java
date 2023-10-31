e6d026db7cda6ee1e73def54d38eef0e9edda91c
joao.victorio <joaovitorvictorio@gmail.com>
2023-10-31 20:15:34 -0300

    Implemented getting posts by id

---
+++ src/main/java/com/victorio/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.victorio.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorio.workshopmongo.domain.Post;
+import com.victorio.workshopmongo.repository.PostRepository;
+import com.victorio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
